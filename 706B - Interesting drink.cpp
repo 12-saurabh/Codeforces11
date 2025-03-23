e3580a6e8b898c397dcc3d21c50e331523d37222
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-03-23 13:20:20 +0530

    Create 706B - Interesting drink.cpp

---
+++ 706B - Interesting drink.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long 
+
+int32_t main(){
+  int n;
+  cin>>n;
+
+  vector<int>v(n);
+
+  for(int i=0;i<n;i++){
+    cin>>v[i];
+  }
+
+  sort(v.begin(),v.end());
+
+  int q;
+  cin>>q;
+
+  while(q--){
+    int x;
+    cin>>x;
+
+    int ans=upper_bound(v.begin(),v.end(),x)-v.begin();
+    cout<<ans<<endl;
+  }
+
+
+  }
+
+
