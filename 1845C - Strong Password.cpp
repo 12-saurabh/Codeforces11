a7fd8fd41655cc4f070894d1fdb6eaebfdd386fc
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-05-19 16:32:35 +0530

    Create 1845C - Strong Password.cpp

---
+++ 1845C - Strong Password.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s_str;
+        cin >> s_str;
+        int n = s_str.size();
+        vector<int> s(n);
+        for (int i = 0; i < n; ++i) {
+            s[i] = s_str[i] - '0';
+        }
+
+        int m;
+        cin >> m;
+
+        string l, r;
+        cin >> l >> r;
+
+        int mx = 0;
+        for (int i = 0; i < m; ++i) {
+            int li = l[i] - '0';
+            int ri = r[i] - '0';
+            int nmx = mx;
+
+            for (int c = li; c <= ri; ++c) {
+                int cur = mx;
+                while (cur < n && s[cur] != c) {
+                    ++cur;
+                }
+                nmx = max(nmx, cur);
+            }
+            mx = nmx + 1;
+        }
+
+        if (mx > n) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
