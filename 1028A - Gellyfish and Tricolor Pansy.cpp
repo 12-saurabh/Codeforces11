b8e328cacb0995737d2693408f528a3e749a5db6
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-06-03 13:58:43 +0530

    Create 1028A - Gellyfish and Tricolor Pansy.cpp

---
+++ 1028A - Gellyfish and Tricolor Pansy.cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        
+
+        if(min(a,c)>=min(b,d)){
+            cout<<"Gellyfish"<<endl;
+        }
+        else{
+            cout<<"Flower"<<endl;
+        }
+
+
+
+
+    }
+}
