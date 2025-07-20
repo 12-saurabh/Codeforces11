6434f0a2a35ec5d04e76ef6fe0c7598e38b71f6c
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-07-20 13:07:02 +0530

    Create A - Greedy Grid.cpp

---
+++ A - Greedy Grid.cpp
@@ -0,0 +1,31 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+int32_t main(){
+    int t;
+    cin>>t;
+    while(t--){
+
+        int n,m;
+        cin>>n>>m;
+
+        if(n==1 || m==1){
+            cout<<"NO"<<endl;
+        }
+
+        else if(n==2 && m==2){
+            cout<<"NO"<<endl;
+        }
+
+        else{
+            cout<<"YES"<<endl;
+        }
+
+
+    }
+    return 0;
+}
