688224bbdc756028ca97005ce8d842c1f005788b
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-09-08 11:57:32 +0530

    Create 1047A - Collatz Conjecture.cpp

---
+++ 1047A - Collatz Conjecture.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+int32_t main() {
+   
+    // For fast run the code
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        
+
+        int k,x;
+        cin>>k>>x;
+
+        cout<<(x<<k)<<endl;
+
+        
+ 
+    }
+    return 0;
+}
